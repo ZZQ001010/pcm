816eb2bbd8984be004e593396417577da91cf96b
wanzhen2 <1105267574@qq.com>
2019-09-01 15:10:03 +0800

    啊

--- pcm-service-impl/build.sh
+++
@@ -1,7 +0,0 @@
-mvn clean install -Dmaven.test.skip=true;
-cd target;
-tar -czf pcm-service-impl.tar.gz *.jar *-dependency/;
-cp pcm-service-impl.tar.gz ../;
-rm -rf *;
-cp ../pcm-service-impl.tar.gz ./;
-
